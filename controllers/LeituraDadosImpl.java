e8c11843fbda6f9499c1ca171d698dc367b915d1
7damner <damneraugusto@gmail.com>
2023-11-20 10:46:14 -0300

    criacao do menu inicial

---
+++ controllers/LeituraDadosImpl.java
@@ -0,0 +1,78 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import models.SiteImpl;
+
+
+    // uma função para abrir o TXT
+    // uma função pra mandar pro TXT
+    // uma função para leitura para cadastro
+    // uma função para leitura do login
+
+public class LeituraDadosImpl {
+
+    public static SiteImpl lerInfoSite() {
+
+        // Especifique o caminho do arquivo que você deseja ler
+            SiteImpl site = null;
+            String caminhoArquivo = "/home/ec2021-fra/ra247035/Documents/testeTxt.txt";
+
+            // Cria um objeto File para representar o arquivo
+            File arquivo = new File(caminhoArquivo);
+    
+            try {
+                // Cria um FileReader para ler o arquivo
+                FileReader leitor = new FileReader(arquivo);
+
+                // Cria um BufferedReader para leitura eficiente
+                BufferedReader bufferedReader = new BufferedReader(leitor);
+    
+                String linha;
+    
+                // Lê cada linha do arquivo até o final
+                while ((linha = bufferedReader.readLine()) != null) {
+                    String[] partes = linha.split(",");
+                
+                    if (partes.length >= 4) {
+                        site = new SiteImpl(partes[0].trim(), partes[1].trim(), partes[2].trim(), partes[3].trim());
+                        System.out.println(linha);
+                    } else {
+                        System.out.println("Linha inválida: " + linha);
+                    }
+                }                
+    
+                // Fecha o BufferedReader
+                bufferedReader.close();
+    
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+           return site;
+        }
+
+
+        public static void dadosLogin(SiteImpl site){
+            Scanner scanner1 = new Scanner(System.in);
+            Scanner scanner2 = new Scanner(System.in);
+
+            System.out.print("Informe o email ");
+            String email  = scanner1.nextLine();
+
+            System.out.print("Informe a senha ");
+            String senha  = scanner2.nextLine();
+
+
+            // vai ter que verificar aqui se o email e a senha está aqui pq nao tem como retornar duas coisas :(
+            // depois vou implementar essa lógica
+
+        }
+
+}
+
+
+ 
---
+++ models/SiteImpl.java
@@ -0,0 +1,27 @@
+// vai guardar as collecions , dados do site 
+
+package models;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SiteImpl {
+    Map<String, String> MapLogin = new HashMap<>();
+    List<String> ListPacotes = new ArrayList<>();
+    List<String> ListReservas = new ArrayList<>();
+    private String nomeEmpresa;
+    private String cpj;
+    private String telefone;
+
+    public SiteImpl(String nomeEmpresa, String cpj, String telefone, String contato) {
+        this.nomeEmpresa = nomeEmpresa;
+        this.cpj = cpj;
+        this.telefone = telefone;
+    }
+
+    public String getNomeEmpresa() { return nomeEmpresa; }
+    public String getCpj() { return cpj; }
+    public String getTelefone() { return telefone;}
+    
+}
