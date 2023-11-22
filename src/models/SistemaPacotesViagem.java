8afc9e2a6fd2d240fab940a478810fda609d8f0e
ArthurKina <tutikina@gmail.com>
2023-11-22 11:45:16 -0300

    adicionei o sistemapacoteviagem que eu tinha deletado

---
+++ src/models/SistemaPacotesViagem.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SistemaPacotesViagem {
+      private ArrayList<Pacote> pacotes;
+      private Scanner scanner;
+
+      // Construtor
+      public SistemaPacotesViagem() {
+          pacotes = new ArrayList<>();
+          scanner = new Scanner(System.in);
+      }
+
+      // Adicionar um pacote de viagem
+      public void criarPacote() {
+
+      }
+
+      // Excluir um pacote de viagem
+      public void excluirPacote() {
+
+      }
+
+      // Editar um pacote de viagem
+      public void editarPacote() {
+
+      }
+
+      // Exibindo todos os pacotes disponíveis
+      public void exibirPacotes() {
+
+      }
+  }
+
