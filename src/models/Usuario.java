9bacefbb5ab914ec2b3819236fd0bfef1920411a
Thiago-nadim <nadimthiago@gmail.com>
2023-11-23 12:03:51 -0300

    fiz uns comentarios

--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class Usuario extends Pessoa {
     private String localizacao;
     List<String> FutViagens = new ArrayList<>();
+    // List<Reserva> historicoReservas = new ArrayList<>();
    
-    public Usuario(boolean ehAdmin, String cpf, String nome, String telefone, String email, String senha) {
+    public Usuario(boolean ehAdmin, String cpf, String nome, String telefone, String email, String senha
+    ) { //String localizacao
+
         super(ehAdmin, cpf, nome, telefone, email, senha);
+        // this.localizacao=localizacao;
     }
     public String getLocalizacao() { return localizacao; }
   //  public String getSenha() { return senha; }
