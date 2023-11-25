28dc3bed7a1458b859313fbf759388cce0d7e750
Thiago-nadim <nadimthiago@gmail.com>
2023-11-25 15:17:23 -0300

    .

--- src/models/Administrador.java
+++ src/models/Administrador.java
@@ -5,6 +5,7 @@ public class Administrador extends Pessoa{
     public Administrador(boolean ehAdmin, String cpf, String nome, String telefone, String email, String senha) {
         super(ehAdmin, cpf, nome, telefone, email, senha);
     }
+
     @Override
     public String toString() {
         
