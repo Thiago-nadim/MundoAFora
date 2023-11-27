a1374adfbd48afbc92930fae5d09c0484a6f9be4
Thiago-nadim <nadimthiago@gmail.com>
2023-11-26 23:28:43 -0300

    .

--- src/controllers/LeituraDadosImpl.java
+++ src/controllers/LeituraDadosImpl.java
@@ -149,7 +149,7 @@ public class LeituraDadosImpl {
                         site.setListPacotes(ListTemporaria);
                         
                     } else {
-                        // System.out.println("Linha inválida: " + linha);
+                        System.out.println("Linha inválida: " + linha);
                     }
                 }                
                 
